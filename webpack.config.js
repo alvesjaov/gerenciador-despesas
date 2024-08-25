e97fa1f5cdefe392540531a2464df2d04fb0e6a0
alvesjaov <victoralves169@outlook.com>
2024-08-25 20:14:36 -0300

    feat: Ajusta configuração do Webpack e adiciona vercel.json para deploy no Vercel
    - Ajusta publicPath no Webpack para garantir caminhos corretos
    - Configura HtmlWebpackPlugin para copiar index.html para a pasta dist
    - Adiciona arquivo vercel.json para configuração de deploy no Vercel
    - Atualiza scripts no package.json para build e start

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,33 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development', // ou 'production' dependendo do seu ambiente
-    entry: './src/ts/app.ts', // Certifique-se de que o caminho está correto
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-        ],
-    },
-    resolve: {
-        extensions: ['.ts', '.js'],
-    },
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'), // Certifique-se de que a pasta dist está na raiz do projeto
-    },
+  mode: 'production', 
+  entry: './src/ts/app.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+  ],
 };
